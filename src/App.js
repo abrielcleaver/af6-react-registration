5965b38475ce038d700f9ef4e78a24be5a1858fb
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-10 15:49:08 -0700

    initialize react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Auth from './views/Authorization/Auth';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
