e5fe293df341a54bed0ae9c966d1796342d4756f
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-12 14:44:00 -0700

    sign in/up set up in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import SignIn from './views/Authorization/SignIn';
+import SignUp from './views/Authorization/SignUp';
+import { useState } from 'react';
+import { getUser } from './services/users';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+
   return (
     <div className="App">
       <BrowserRouter>
+        <NavLink to="/signin">Sign In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/signin">
             <SignIn />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/">
+            {currentUser && <h1>I&apos;m signed in</h1>}
+            {!currentUser && <h1>I&apos;m not signed in</h1>}
           </Route>
         </Switch>
       </BrowserRouter>
